db9f8b38d0b6cc95a94f45a4917b6c14053b64fc
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-06-29 21:39:39 +0530

    Add files via upload

---
+++ raj.js
@@ -0,0 +1,24 @@
+function myFun1(){  
+    document.getElementById('ileft').style.backgroundImage = "url(assets/homelights/hl1.jpg)";
+    document.getElementById('imid').style.backgroundImage = "url(assets/homelights/hm2.webp)";
+    document.getElementById('iright').style.backgroundImage = "url(assets/homelights/hm3.jpg)";
+}
+
+function myFun2(){
+    document.getElementById('ileft').style.backgroundImage = "url(assets/officelights/ol1.jpg)";
+    document.getElementById('imid').style.backgroundImage = "url(assets/officelights/ol2.jpg)";
+    document.getElementById('iright').style.backgroundImage = "url(assets/officelights/ol3.webp)";
+}
+
+function myFun3(){
+    document.getElementById('ileft').style.backgroundImage = "url(assets/publights/pl1.jpg)";
+    document.getElementById('imid').style.backgroundImage = "url(assets/publights/pl2.png)";
+    document.getElementById('iright').style.backgroundImage = "url(assets/publights/pl3.jpg)";
+}
+
+function myFun4(){
+    document.getElementById('ileft').style.backgroundImage = "url(assets/bedlights/bl1.jpg)";
+    document.getElementById('imid').style.backgroundImage = "url(assets/bedlights/bl2.jpg)";
+    document.getElementById('iright').style.backgroundImage = "url(assets/bedlights/bl3.jpg)";
+}
+
